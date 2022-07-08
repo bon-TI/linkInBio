c0c0d02abe1937064cba330d0d9ce6a321e9b0b1
bon <joseipaz1203@gmail.com>
2022-07-08 12:10:22 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var options = {
+    strings: ["< JOSE BOLIVAR />", "FRONT-END DEVELOPER", "WEB DESIGNER"], 
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+};
+
+var typed = new Typed(".auto-typed", options);
